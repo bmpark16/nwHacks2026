124f795978151da5df3a74f7c15f0495585b5349
fyip3 <flemingyip99@gmail.com>
2026-01-18 11:33:13 -0800

    final arduino changes

--- arduino/servo_controller/servo_controller.ino
+++ arduino/servo_controller/servo_controller.ino
@@ -4,7 +4,7 @@ Servo myServo;
 const int SERVO_PIN = 9;  // Connect servo to digital pin 9
 const int START_POSITION = 0;
 const int END_POSITION = 105;
-const int SWEEP_DELAY = 15;  // Delay between each degree (ms)
+const int SWEEP_DELAY = 5;  // Delay between each degree (ms)
 const int RESET_DELAY = 500;  // Delay before resetting (ms)
 
 bool autoMode = false;
